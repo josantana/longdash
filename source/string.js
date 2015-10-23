0415685e14d0f0c25db68b5668ce648e6621d48b
Jo Santana <josantana@jogalabs.com>
2015-10-23 18:08:06 -0200

    NEW: String Module
    - Added two methods: serialize / slugfy;

---
+++ source/string.js
@@ -0,0 +1,66 @@
+
+//  Namespace: __
+//  Element: string
+
+(function()
+{
+    'use strict';
+
+    // Declare root variable - window in the browser, global on the server
+    // Get already define BAT object (if available) or create a new object
+
+    var root = this,
+        module = {};
+
+    module.string = (function ()
+    {
+        var exports = {};
+
+        exports.serialize = function (obj) {
+
+            var data = '';
+
+            for (var key in obj) {
+                data += key + '=' + encodeURIComponent(obj[key]) + '&';
+            }
+
+            data = data.slice(0,-1);
+
+            return data;
+
+        };
+
+        exports.slugfy = function (string, noWhitespace) {
+
+            var slug = string.toLowerCase();
+
+            noWhitespace = noWhitespace ? noWhitespace : false;
+
+            slug = slug.replace(new RegExp(/\s/g),'-');
+            slug = slug.replace(new RegExp(/[àáâãäå]/g),'a');
+            slug = slug.replace(new RegExp(/æ/g),'ae');
+            slug = slug.replace(new RegExp(/ç/g),'c');
+            slug = slug.replace(new RegExp(/[èéêë]/g),'e');
+            slug = slug.replace(new RegExp(/[ìíîï]/g),'i');
+            slug = slug.replace(new RegExp(/ñ/g),'n');
+            slug = slug.replace(new RegExp(/[òóôõö]/g),'o');
+            slug = slug.replace(new RegExp(/œ/g),'oe');
+            slug = slug.replace(new RegExp(/[ùúûü]/g),'u');
+            slug = slug.replace(new RegExp(/[ýÿ]/g),'y');
+
+            if (noWhitespace) {
+                slug = slug.replace(new RegExp(/\W/g),'');
+            }
+
+            return slug;
+        }
+
+        return exports;
+
+    })();
+
+    // Export module to root under(long)line object
+
+    root.__ = root._.extend((root.__ || _), module);
+
+}).call(this);

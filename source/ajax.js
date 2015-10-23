7d7704f326281d7bc01756558492fa4687e9677b
Jo Santana <josantana@jogalabs.com>
2015-10-23 18:10:36 -0200

    UPDATE: GET
    - GET method is working as expected.
    - Added function to join parameters.
    - Fixed status codes and return.

--- source/ajax.js
+++ source/ajax.js
@@ -17,6 +17,7 @@
         var exports = {};
 
         var parse = function (req) {
+
             var result;
 
             try {
@@ -54,23 +55,29 @@
             }
 
             // Remove hash and add protocol if not provided (prefilters might expect it)
-            // We also use the url parameter if available
 
             options.url = ((options.url || location.href) + '').replace(/#.*$/, '').replace(/^\/\//, location.protocol + '//');
 
+            // Glue parameters
+
+            if (options.params) {
+                var glue = options.url.match(/\?/g) ? '&' : '?';
+                options.url = options.url + glue + root.__.string.serialize(options.params);
+            }
+
             // Open connection
 
-            request.open(type, url, true);
+            request.open(type, options.url, true);
 
             // If method is POST or PUT
 
             if (type.match(/POST|PUT/g)) {
-                request.setRequestHeader('Content-type', 'application/x-www-form-urlencoded; charset=UTF-8');
+                request.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
             }
 
             // If we got headers, set them
 
-            if (options.headers && options.headers.length) {
+            if (options.headers) {
 
                 for (var key in options.headers) {
                     request.setRequestHeader(key, options.headers[key]);
@@ -82,7 +89,7 @@
             request.onreadystatechange = function () {
 
                 if (request.readyState === 4) {
-                    if (request.status >= 200 && request.status < 400) {
+                    if (request.status >= 200 && request.status < 300) {
                         methods.success.apply(methods, parse(request));
                     } else {
                         methods.error.apply(methods, parse(request));
@@ -95,11 +102,11 @@
             var callbacks = {
                 success: function (callback) {
                     methods.success = callback;
-                    return methods;
+                    return callbacks;
                 },
                 error: function (callback) {
                     methods.error = callback;
-                    return methods;
+                    return callbacks;
                 }
             };
 

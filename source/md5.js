519a1b68217979ed58f66311fe5bc472b9b6618e
Jo Santana <josantana@jogalabs.com>
2015-11-21 20:57:59 -0200

    NEW: MD5 Module

---
+++ source/md5.js
@@ -0,0 +1,32 @@
+
+//  Namespace: __
+//  Module: md5
+
+(function()
+{
+    'use strict';
+
+    // Declare root variable
+
+    var root = window,
+        module = {};
+
+    module.md5 = (function ()
+    {
+        var hash;
+
+        // Helpers
+        //import("md5/setup.js");
+
+        // Methods
+        //import("md5/hash.js");
+
+        return hash;
+
+    })();
+
+    // Export module to root Longdash object
+
+    root.__ = root._.extend((root.__ || root._), module);
+
+}).call(this);

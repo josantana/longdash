6cdcfd4e57425963383d310886febed82cab462a
Jo Santana <josantana@jogalabs.com>
2015-12-01 18:40:52 -0200

    Revert "UPDATED test using BrowserStack and QUnit"
    This reverts commit 8e1433bd8436753bd8c36da8466da8a2841bf7b2.

--- test/test.js
+++
@@ -1,9 +0,0 @@
-function isOdd (val) {
-    return val % 2 === 1;
-}
-
-test('isOdd()', function() {
-    ok(isOdd(1), 'One is an odd number');
-    ok(isOdd(3), 'Three is an odd number');
-    ok(!isOdd(0), 'Zero is not odd number');
-});
